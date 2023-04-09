8ae59441d20c20ba81606b8bd362e9c8cd1fba2e
kirtana55 <kirtanapatell55@gmail.com>
2023-04-09 02:20:37 +0100

    Week10project1

---
+++ src/nopcommerce/MultiBrowser.java
@@ -0,0 +1,53 @@
+package nopcommerce;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MultiBrowser {
+    static String browser = "Firefox";
+
+    static String baseUrl = "https://demo.nopcommerce.com/login?returnUrl=%2/";
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        } else {
+            System.out.println("Wrong Browser name");
+        }
+
+        //open the URL into Browser
+        driver.get(baseUrl);
+
+        //Get the Title of the page
+        System.out.println("Title of the page : " + driver.getTitle());
+
+        //Get the Current URL
+        System.out.println("Current URL : " + driver.getCurrentUrl());
+
+        // Get Page Source
+        System.out.println("Page source : " + driver.getPageSource());
+
+        // Find the Email field element
+        WebElement emailField = driver.findElement(By.id("Email"));
+
+        //Type the Email address to email field element
+        emailField.sendKeys("prime123@gmail.com");
+
+        // Find the password field Element and Type the password
+        driver.findElement(By.name("Password")).sendKeys("Prime123");
+
+        // Close the Browser
+        // driver.close();
+
+    }
+
+}
